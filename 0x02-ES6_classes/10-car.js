7ced070e2e2484c105f857c3e8940ac89f59fce6
deantosh <deantoshdaiddoh@gmail.com>
2024-10-04 19:05:35 +0000

    task 10: solution added

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,29 +1,41 @@
-// Define unique symbols for each attribute
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
-    this._brand = Car._validateString(brand, 'brand');
-    this._motor = Car._validateString(motor, 'motor');
-    this._color = Car._validateString(color, 'color');
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
   }
 
   // Method to clone the Car object
   cloneCar() {
-      const clone = new this.constructor('', '', '');
-    clone._brand = undefined;
-    clone._motor = undefined;
-    clone._color = undefined;
-    return clone;
-  }
-
-  // Validate functions
-  static _validateString(value, attributeName) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${attributeName} must be a string`);
-    }
-    return value;
+    const Clone = this.constructor[Symbol.species];
+    return new Clone();
   }
 }
