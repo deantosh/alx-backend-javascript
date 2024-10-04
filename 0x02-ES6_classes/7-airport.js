7ec011b505cf7bd4bdec5d3114c2732c62acd349
deantosh <deantoshdaiddoh@gmail.com>
2024-10-04 10:54:52 +0000

    task 7: solution added

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,12 +1,13 @@
 export default class Airport {
   constructor(name, code) {
     this._name = Airport._validateString(name, 'name');
-	this._code = Airport._validateString(code, 'code');
+    this._code = Airport._validateString(code, 'code');
   }
-  
+
   toString() {
     return `[Object ${this._code}]`;
   }
+
   // Validate functions
   static _validateString(value, attributeName) {
     if (typeof value !== 'string') {
