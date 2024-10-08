fdf719c3fe9e98d92be7c9b6d7318f31a24b41eb
deantosh <deantoshdaiddoh@gmail.com>
2024-10-08 17:44:49 +0000

    task 10: solution added

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,14 @@
+// Create a function named updateUniqueItems
+//  - returns an updated map for all items with initial quantity at 1.
+
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+  return map;
+}
