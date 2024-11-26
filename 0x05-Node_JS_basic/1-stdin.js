02e0525f2f92aa6ca6776e73d6e6f3fe02b8cd9b
deantosh <deantoshdaiddoh@gmail.com>
2024-11-26 20:10:06 +0000

    task 1: solution added

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,24 @@
+// Create a program named 1-stdin.js that will be executed through command line:
+
+const readline = require('readline');
+
+// Create interface for user input
+const r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Display initial message
+console.log('Welcome to Holberton School, what is your name?');
+
+// Handle user input
+r1.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  r1.close();
+});
+
+// Handle program close
+r1.on('close', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
+});
