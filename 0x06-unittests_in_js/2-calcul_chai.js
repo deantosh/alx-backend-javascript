1dd8d83fe9ae2ef7f2c7ef1f98b6dd1828f8eda7
deantosh <deantoshdaiddoh@gmail.com>
2024-11-28 18:39:35 +0000

    task 2: solution added

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,22 @@
+// Function that adds, subtracts and divides 2 numbers based on the type operation.
+
+function calculateNumber(type, a, b) {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  }
+  else if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+  else if (type === 'DIVIDE') {
+    b = Math.round(b);
+    if (b === 0) {
+      return 'Error';
+    }
+    return Math.round(a) / Math.round(b);
+  }
+  else {
+    return 'Error';
+  }
+}
+
+module.exports = calculateNumber;
