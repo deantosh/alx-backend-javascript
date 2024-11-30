e8d35033cd9c50c73895b39a42b125a43abfdf03
deantosh <deantoshdaiddoh@gmail.com>
2024-11-30 14:02:48 +0000

    task 6: solution added

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,9 @@
+// Working with async testing.
+
+function getPaymentTokenFromAPI(success) {
+  if (success) {
+      return Promise.resolve({data: 'Successful response from the API' })
+  }
+}
+
+module.exports = getPaymentTokenFromAPI;
